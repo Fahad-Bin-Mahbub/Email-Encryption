60253975a2a095086dacbaa1dc10b698276cdccb
Fahad Bin Mahbub <philomath@Fahads-MacBook-Air-2.local>
2025-07-01 03:46:01 +0600

    Added

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OQdDqzxPKhy8evQ4dfDihyPQ/Ii/vQRa71e/6ZVacuc=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+import EmailEncryption from "./EmailEncryption";
+
+export default function Home() {
+	return <EmailEncryption/>;
+}
